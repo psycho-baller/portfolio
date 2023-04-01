9f3c691db0a00da06cde138dd24dabd3accc4391
Rami Maalouf <rami.maalouf@ibm.com>
2023-04-01 18:11:56 +0400

    got a solar system, now I wanna make react

--- src/components/home/Ball.tsx
+++ src/components/home/Ball.tsx
@@ -1,24 +1,52 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { Quaternion, Vector3, Euler, Group, Mesh, MathUtils } from "three";
 
-export default function Ball(props: any) {
-  const mesh = useRef<THREE.Mesh>(null!);
+export default function Ball({
+  ballGroup,
+  ballRef,
+  position = [0, 0, 0],
+  tiltAxis = [0, 0, 0],
+  initialAngle = [0, 0, 0],
+  ...props
+}: {
+  ballGroup: React.MutableRefObject<Group>;
+  ballRef: React.MutableRefObject<Mesh>;
+  position?: [number, number, number];
+  tiltAxis?: [number, number, number];
+  initialAngle?: [number, number, number];
+  [x: string]: any;
+}) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  useFrame((state, delta) => {
-    // console.log(state);
+
+  const sunPosition = new Vector3(0, 0, 0); // center
+  const distanceFromRadius = 5;
+  // const angle = Math.PI / 8;
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    // ballGroup.current.rotation.y += 0.01; // rotate the group around the Y-axis
+    // ballRef.current.rotation.y += 0.01; // rotate the earth mesh around its own axis
+    // ballRef.current.rotation.z = angle + elapsedTime; // rotate the earth mesh at an angle relative to its own axis
+    ballRef.current.position.set(
+      distanceFromRadius * Math.cos(elapsedTime) + position[0],
+      position[1],
+      distanceFromRadius * Math.sin(elapsedTime) + position[2]
+    );
   });
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <sphereGeometry args={[0.5, 32, 32]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh rotation={tiltAxis}>
+      <mesh
+        {...props}
+        ref={ballRef}
+        scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <sphereGeometry args={[0.5, 32, 32]} />
+        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      </mesh>
     </mesh>
   );
 }
--- src/components/home/Balls.tsx
+++ src/components/home/Balls.tsx
@@ -1,7 +1,6 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import Ball from "./Ball";
-import { Quaternion, Vector3 } from "three";
 
 export default function Balls(props: any) {
   const ball1 = useRef<THREE.Mesh>(null!);
@@ -12,9 +11,27 @@ export default function Balls(props: any) {
   return (
     <group {...props} ref={ballGroup}>
       {/* hover only one ball at a time */}
-      <Ball ref={ball1} position={[-2, 0, 0]} />
-      <Ball ref={ball2} position={[0, 2, 0]} />
-      <Ball ref={ball3} position={[0, 0, 4]} />
+      <Ball
+        ballRef={ball1}
+        ballGroup={ballGroup}
+        // position={[-2, 0, 0]}
+        tiltAxis={[Math.PI / 8, 0, 0]}
+        initialAngle={[0, 0, 0]}
+      />
+      <Ball
+        ballRef={ball2}
+        ballGroup={ballGroup}
+        // position={[5, 2, 3]}
+        // tiltAxis={[0, Math.PI / 8, 0]}
+        initialAngle={[0, 0, 0]}
+      />
+      <Ball
+        ballRef={ball3}
+        ballGroup={ballGroup}
+        // position={[0, 0, 2]}
+        tiltAxis={[0, 0, Math.PI / 8]}
+        initialAngle={[0, 0, 0]}
+      />
     </group>
   );
 }

46ddbe011fb7fe842a31e60f36686dbed3f2f94d
Rami Maalouf <rami.maalouf@ibm.com>
2023-04-03 19:27:54 +0400

    rotation and hover effect complete

--- src/components/home/Ball.tsx
+++ src/components/home/Ball.tsx
@@ -1,62 +1,90 @@
 import { Line } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { Quaternion, Vector3, Euler, Group, Mesh, MathUtils, BufferGeometry, Float32BufferAttribute } from "three";
 import { useControls } from "leva";
 
 export default function Ball({
-  ballGroup,
-  ballRef,
+  // ballGroup,
+  // ballRef,
   tiltAxis = [0, 0, 0],
-  initialAngle = [0, 0, 0],
+  initialAngle = 0,
   ...props
 }: {
-  ballGroup: React.MutableRefObject<Group>;
-  ballRef: React.MutableRefObject<Mesh>;
+  // ballGroup: React.MutableRefObject<Group>;
+  // ballRef: React.MutableRefObject<Mesh>;
   tiltAxis?: [number, number, number];
-  initialAngle?: [number, number, number];
+  initialAngle?: number;
   [x: string]: any;
 }) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
+  const ballGroup = useRef<Group>(null!);
+  const ballRef = useRef<Mesh>(null!);
+
   // check if page is in debug mode
   const debug = window.location.href.includes("debug");
   const { speed } = (debug && useControls({ speed: { value: 10, min: 0, max: 1000 } })) || { speed: 10 };
 
-  // const [speed, setSpeed] = useState(500);
+  // const [speed, setSpeed] = useState(5);
+
+  // var speed = 5;
 
   const sunPosition = new Vector3(0, 0, 0); // center
-  const distanceFromRadius = 5;
+  const distanceFromRadius = 8;
   const path = new BufferGeometry();
   const positions = [] as number[];
+  let initialSlowness = 0.001;
+  let initialFastness = 0.007;
+  // const speed = 5;
   // const angle = Math.PI / 8;
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
-    // ballGroup.current.rotation.y += 0.01; // rotate the group around the Y-axis
-    // ballRef.current.rotation.y += 0.01; // rotate the earth mesh around its own axis
-    // ballRef.current.rotation.z = angle + elapsedTime; // rotate the earth mesh at an angle relative to its own axis
-    ballRef.current.position.x = distanceFromRadius * Math.cos(elapsedTime * speed);
-    ballRef.current.position.z = distanceFromRadius * Math.sin(elapsedTime * speed);
 
+    // if user hovers on a ball, rotate the ball group the opposite direction of the ball (slows it down)
+    if (hovered) {
+      ballGroup.current.rotateOnAxis(new Vector3(0, 1, 0), initialSlowness);
+      // gradually increase the speed
+      if (initialSlowness < initialFastness) {
+        initialSlowness += 0.00008; // gradually increase the opposing speed
+      }
+    } else {
+      // if user is not hovering on a ball, slowly reduce the speed of the ball group (speed up the ball)
+      // had to introduce the initialFastness variable coz when we hover, state is changed and the initialSlowness is reset to 0
+      if (initialFastness > initialSlowness) {
+        ballGroup.current.rotateOnAxis(new Vector3(0, 1, 0), initialFastness);
+        initialFastness -= 0.00008; // gradually decrease the opposing speed
+      }
+    }
+
+    // rotate the ball all the time
+    ballRef.current.position.x = Math.cos(elapsedTime + initialAngle) * distanceFromRadius;
+    ballRef.current.position.z = Math.sin(elapsedTime + initialAngle) * distanceFromRadius;
     positions.push(ballRef.current.position.x, ballRef.current.position.y, ballRef.current.position.z);
     path.setAttribute("position", new Float32BufferAttribute(positions, 3));
   });
   return (
-    <group rotation={tiltAxis}>
-      {/* plane rotated 90 degrees to the x-axis */}
-      {/* <boxGeometry args={[10, 0, 10]} /> */}
-      {/* <meshBasicMaterial color="red" /> */}
+    <group rotation={tiltAxis} ref={ballGroup}>
       <mesh
         {...props}
         ref={ballRef}
         scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
+        onClick={(_event) => {
+          setActive(!active);
+        }}
+        onPointerOver={(_event) => {
+          setHover(true);
+          // setSpeed(1);
+        }}
+        onPointerOut={(_event) => {
+          setHover(false);
+          // setSpeed(5);
+        }}
       >
-        <sphereGeometry args={[0.5, 32, 32]} />
+        <sphereGeometry args={[1, 32, 32]} />
         <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
       </mesh>
+
       <line>
         <bufferGeometry attach="geometry" {...path} />
         <lineBasicMaterial attach="material" color="#61FFFB" />
--- src/components/home/Balls.tsx
+++ src/components/home/Balls.tsx
@@ -3,17 +3,14 @@ import { useRef, useState } from "react";
 import Ball from "./Ball";
 
 export default function Balls(props: any) {
-  const ball1 = useRef<THREE.Mesh>(null!);
-  const ball2 = useRef<THREE.Mesh>(null!);
-  const ball3 = useRef<THREE.Mesh>(null!);
   const ballGroup = useRef<THREE.Group>(null!);
 
   return (
     <group {...props} ref={ballGroup}>
       {/* hover only one ball at a time */}
-      <Ball ballRef={ball1} ballGroup={ballGroup} tiltAxis={[Math.PI / 15, 0, 0]} initialAngle={[0, 0, 0]} />
-      <Ball ballRef={ball2} ballGroup={ballGroup} tiltAxis={[Math.PI / 6, 0, -Math.PI / 3]} initialAngle={[0, 0, 0]} />
-      <Ball ballRef={ball3} ballGroup={ballGroup} tiltAxis={[Math.PI / 6, 0, Math.PI / 3]} initialAngle={[0, 0, 0]} />
+      <Ball tiltAxis={[Math.PI / 15, 0, 0]} initialAngle={-Math.PI / 2} />
+      <Ball tiltAxis={[Math.PI / 15, 0, -Math.PI / 4]} initialAngle={Math.PI / 2} />
+      <Ball tiltAxis={[Math.PI / 15, 0, (5 * Math.PI) / 4]} initialAngle={Math.PI / 2} />
     </group>
   );
 }

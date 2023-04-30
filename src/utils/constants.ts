293abd9549de12124961d79e36d7aa0ea8a6f9fc
Rami Maalouf <rami.maalouf@ibm.com>
2023-04-30 14:17:15 +0400

    grammar

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -45,8 +45,8 @@ export const externalLinks = [
 
 export const messages = {
     "snipd-team": {
-        header: "Hey Snipd",
-        body: `1: I amazed and in love with Snipd
+        header: "Hey Snipd!",
+        body: `1: I amazed by and in love with Snipd
 2: A huge fan of what you 5 are doing and I would love to be a +1 to the team, but before that,
 4: I'm transiting through ZRH(layover 6:10-13:25; may 10), and will try to leave the airport and if that's possible I would love to meet you guys in person. I wanna learn about you guys' journey and if you want we could make a podcast episode (and maybe catch a game of table tennis😉)
 5: Why I'm a great fit: I am currently doing a SWE internship @IBM and will be done exactly one year from now. Currently I wouldn't really provide unique benefit by joining your team but I will be in a year, I have a few project ideas related to AI that I am working on almost everyday. Right now I can mainly offer web dev (3D, front-end, back-end). You can find more info about what I build in my [github](https://github.com/psycho-baller)

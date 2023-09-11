dea7d982233c819522522f81a0d7fd28ef70915b
Rami Maalouf <rami.maalouf@ibm.com>
2023-09-11 09:30:16 -0400

    revert coz im dumb

--- src/components/solid/NavLinks.tsx
+++ src/components/solid/NavLinks.tsx
@@ -1,12 +1,10 @@
 // import "solid-js";
 /** @jsxImportSource solid-js */
-import { For, createMemo, createSignal, onMount } from "solid-js";
+import { For, createSignal, onMount } from "solid-js";
 import { navLinks } from "@utils/constants";
 import "@styles/logo.css";
 export default function Navbar() {
   const [active, setActive] = createSignal("/");
-  const burger = createMemo(() => document.querySelector(".burger"));
-
 
   onMount(() => {
     setActive(window.location.pathname);
@@ -28,8 +26,9 @@ export default function Navbar() {
                 active() === link.href ? "text-cyan-400" : "text-cyan-100"
               } capitalize hover:text-cyan-300 text-[18px] font-medium cursor-pointer block sm:inline-block`}
               onClick={() => {
-                if (burger()?.classList.contains("active") && window.innerWidth < 640 && active() !== link.href){
-                  burger()?.classList.remove("active");
+                const burger = document.querySelector(".burger");
+                if (burger?.classList.contains("active") && window.innerWidth < 640 && active() !== link.href){
+                  burger.classList.remove("active");
                 }
               }}
             >

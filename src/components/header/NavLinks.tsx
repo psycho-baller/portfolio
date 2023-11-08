a34c3eb5036f9ed9012ad569b5081096ff68c8fb
Rami Maalouf <rami.maalouf@ibm.com>
2023-11-07 23:20:53 -0500

    improve navbar(smaller mainly) even more (less spacing)

--- src/components/header/NavLinks.tsx
+++ src/components/header/NavLinks.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
       class="sm:fixed sm:top-5 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:z-50"
       id="nav-links"
     >
-      <ul class="sm:flex sm:gap-x-1 lg:gap-x-5 sm:backdrop-blur-3xl sm:rounded-full sm:p-2.5 transition ease-in-out sm:bg-cyan-300/10">
+      <ul class="sm:flex sm:gap-x-1 lg:gap-x-2.5 sm:backdrop-blur-3xl sm:rounded-full sm:py-2.5 sm:px-3 transition ease-in-out sm:bg-cyan-300/10">
         <For each={navLinks}>
           {(link) => (
             <li
@@ -24,8 +24,8 @@ export default function Navbar() {
               // TODO: for some reason JS not working
               // TODO: reverse the hover effect
               class={`${
-                active() === link.href ? "sm:bg-cyan-400/10" : ""
-              } capitalize text-cyan-100 sm:rounded-full cursor-pointer transition duration-200 ease-in-out sm:px-3 md:px-4 lg:px-5 sm:py-1 md:py-1.5 sm:hover:bg-cyan-400/5`}
+                active() === link.href ? "sm:bg-cyan-400/10" : " sm:hover:bg-cyan-400/5"
+              } capitalize text-cyan-100 sm:rounded-full cursor-pointer transition duration-200 ease-in-out sm:px-3 md:px-4 lg:px-5 sm:py-1 md:py-[0.3rem]`}
               onClick={() => {
                 // if clicked on the active link, close the menu
                 // if (active() === link.href) {

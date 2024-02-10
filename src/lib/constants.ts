b2eb1744211027d61db8fa71da7b443bcce9923f
Rami Maalouf <rami.maalouf@ibm.com>
2024-02-09 19:22:19 -0500

    sub_confirmation=1

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,69 +1,71 @@
-import type { Message } from "./types";
+import type { Message } from './types';
+
+export const BASE_URL = import.meta.env.BASE_URL;
 
 export const navLinks = [
   {
-    href: "/blog",
-    title: "Blog",
+    href: '/blog',
+    title: 'Blog',
   },
   {
-    href: "/about",
-    title: "About",
+    href: '/about',
+    title: 'About',
   },
   {
-    href: "/contact",
-    title: "Contact",
+    href: '/contact',
+    title: 'Contact',
   },
   {
-    href: "/support",
-    title: "Support",
+    href: '/support',
+    title: 'Support',
   },
   {
-    href: "/resume",
-    title: "Resume",
+    href: '/resume',
+    title: 'Resume',
   },
 ];
 export const externalLinks = {
-  github: "https://github.com/psycho-baller",
-  youtube: "https://youtube.com/@ramimaalouf",
-  linkedin: "https://www.linkedin.com/in/rami--maalouf/",
-  strava: "https://www.strava.com/athletes/50533842",
-  email: "mailto:ramim66809@gmail.com",
-  goodreads: "https://www.goodreads.com/user/show/161793210-rami",
-  resume: "https://docs.google.com/document/d/1tY_v0Se3EH0pbjVZ5-hqM8I8uq0jK4ctuRLBOG4_JrI/edit?usp=sharing",
+  github: 'https://github.com/psycho-baller',
+  youtube: 'https://youtube.com/@ramimaalouf?sub_confirmation=1',
+  linkedin: 'https://www.linkedin.com/in/rami--maalouf/',
+  strava: 'https://www.strava.com/athletes/50533842',
+  email: 'https://mail.google.com/mail/u/0/?fs=1&to=ramim66809@gmail.com&su=About...&body=Hey%20Rami,%20....&tf=cm',
+  goodreads: 'https://www.goodreads.com/user/show/161793210-rami',
+  resume: 'https://docs.google.com/document/d/1tY_v0Se3EH0pbjVZ5-hqM8I8uq0jK4ctuRLBOG4_JrI/edit?usp=sharing',
 } as const;
 
 export const externalFooterLinks = [
   {
-    iconName: "mdi:github",
+    iconName: 'mdi:github',
     href: externalLinks.github,
-    name: "Link to my Github",
+    name: 'Link to my Github',
   },
   {
-    iconName: "ant-design:youtube-filled",
-    href: "https://youtube.com/@ramimaalouf",
-    name: "Link to my Youtube",
+    iconName: 'ant-design:youtube-filled',
+    href: externalLinks.youtube,
+    name: 'Link to my Youtube',
   },
   {
-    iconName: "mdi:linkedin",
-    href: "https://www.linkedin.com/in/rami--maalouf/",
-    name: "Link to my LinkedIn",
+    iconName: 'mdi:linkedin',
+    href: externalLinks.linkedin,
+    name: 'Link to my LinkedIn',
   },
   {
-    iconName: "ic:round-email",
-    href: "https://mail.google.com/mail/u/0/?fs=1&to=rami.rami@ucalgary.ca&su=About...&body=Hey%20Rami,%20....&tf=cm",
-    name: "Link to Email me",
+    iconName: 'ic:round-email',
+    href: externalLinks.email,
+    name: 'Link to Email me',
   },
   {
-    iconName: "simple-icons:strava",
-    href: "https://www.strava.com/athletes/50533842",
-    name: "Link to my Strava",
+    iconName: 'simple-icons:strava',
+    href: externalLinks.strava,
+    name: 'Link to my Strava',
   },
 ];
 
 // TODO: use Notion instead as a CMS
 export const messages = {
-  "snipd-team": {
-    header: "Hey Snipd!",
+  'snipd-team': {
+    header: 'Hey Snipd!',
     body: `1: I amazed by and in love with Snipd
 2: A huge fan of what you 5 are doing and I would love to be a +1 to the team, but before that,
 4: I'm transiting through ZRH(layover 6:10-13:25; may 10), and will try to leave the airport and if that's possible I would love to meet you guys in person. I wanna learn about you guys' journey and if you want we could make a podcast episode (and maybe catch a game of table tennis😉)
@@ -71,7 +73,7 @@ export const messages = {
 6: Thank you for every second you guys put into Snipd... and reading this message
 P.S: I would love to meet up with all of you but I'm also down to meet with whoever is available and willing to meet up`,
     actionButton1: {
-      text: "Email me",
+      text: 'Email me',
       url: "https://mail.google.com/mail/u/0/?fs=1&to=rami.rami@ucalgary.ca&su=Snipd meetup&body=Hey%20Rami,%0A%0AYeah sure we'd love to meet up...&tf=cm",
     },
   },

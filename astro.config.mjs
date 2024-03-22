166913313f2d601290b638e89e21ef0b7aec730b
Rami Maalouf <rami.maalouf@ibm.com>
2024-03-22 01:57:02 -0400

    bump to v4 of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,14 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ramimaalouf.tech',
+  site: 'https://www.rami-maalouf.tech',
   redirects:
   {
     "/resume": "https://docs.google.com/document/d/1tY_v0Se3EH0pbjVZ5-hqM8I8uq0jK4ctuRLBOG4_JrI/edit?usp=sharing",
     "/linkedin": "https://www.linkedin.com/in/rami--maalouf/"
   },
   integrations: [
-    react({
-      include: ["**/react/*"],
-    }),
+    react(),
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
@@ -28,9 +26,7 @@ export default defineConfig({
       include: ["**/NavLinks.tsx"]
     })
   ],
-  experimental: {
-    devOverlay: true
-  },
+  devOverlay: true,
   // output: "server",
   // adapter: vercel(),
   server: {
